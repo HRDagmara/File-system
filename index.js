53c91c2bea1a019940bdf4cd47801fdb59378af7
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-16 20:58:09 +0100

    Add fs readdir in index.js

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 var fs = require('fs');
 var StatMode = require('stat-mode');
+var colors = require('colors');
+
+fs.readFile('./text.txt', 'utf-8', function(err, data) {
+	console.log('Dane przed zapisem!'.blue);
+    console.log(data);
+    fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function(err) {
+    	if (err) throw err; 
+    	console.log('Zapisano!'.blue);
+    	fs.readFile('./text.txt', 'utf-8', function(err, data) {
+    		console.log('Dane po zapisie!'.blue);
+    		console.log(data);	
+    	});
+	});
+});
 
 fs.stat('./cat.jpg', function(err, stats) {
     var statMode = new StatMode(stats);
     console.log(statMode.toString());
 });
+
+fs.readdir('../File system', 'utf-8', function(err, files) {
+	console.log('files');
+
+	for (var i=0; i<files.length; i++) {
+		console.log(files[i]);
+	}
+
+	fs.writeFile('message.txt', [files], function(err) {
+  		if (err) throw err;
+  		console.log('Zapisano!');
+    	fs.readdir('./message.txt', 'utf-8', function(err, data) {
+    		console.log(data);	
+    	});
+	});
+});
+
+
+
+
+
+
