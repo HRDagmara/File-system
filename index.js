c5963ea602d1d075d4469a6da3de8fb8523c0ebc
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-19 15:04:14 +0100

    Add index.js

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ var StatMode = require('stat-mode');
 var colors = require('colors');
 
 fs.readFile('./text.txt', 'utf-8', function(err, data) {
+
 	console.log('Dane przed zapisem!'.blue);
     console.log(data);
+
     fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function(err) {
+
     	if (err) throw err; 
     	console.log('Zapisano!'.blue);
-    	fs.readFile('./text.txt', 'utf-8', function(err, data) {
+    	
+        fs.readFile('./text.txt', 'utf-8', function(err, data) {
+
     		console.log('Dane po zapisie!'.blue);
     		console.log(data);	
     	});
@@ -16,19 +21,27 @@ fs.readFile('./text.txt', 'utf-8', function(err, data) {
 });
 
 fs.stat('./cat.jpg', function(err, stats) {
+
     var statMode = new StatMode(stats);
     console.log(statMode.toString());
 });
 
 fs.readdir('../File system', 'utf-8', function(err, files) {
+
     console.log('files');
-	for (var i=0; i<files.length; i++) {
+	
+    for (var i=0; i<files.length; i++) {
+
 		console.log(files[i]);
 	}
-	fs.writeFile('message.txt', [files], function(err) {
+	
+    fs.writeFile('message.txt', [files], function(err) {
+
   		if (err) throw err;
   		console.log('Zapisano!');
-      	fs.readdir('./message.txt', 'utf-8', function(err, data) {
+      	
+        fs.readdir('./message.txt', 'utf-8', function(err, data) {
+
     		console.log(data);	
     	});
 	});
